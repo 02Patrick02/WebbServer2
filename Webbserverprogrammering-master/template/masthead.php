fd8ccf28a083f44439a82e72ce3d5a95c30bce49
02Patrick02 <54435353+02Patrick02@users.noreply.github.com>
2020-11-13 11:17:28 +0100

    Add files via upload

---
+++ Webbserverprogrammering-master/template/masthead.php
@@ -0,0 +1,7 @@
+<?php
+
+?>
+
+ <header><!--Sidhuvud-->
+            <h1>Min onlinebutik - <?php echo $h1 span; ?></h1>
+  </header>
---
+++ Webbserverprogrammering-master/template/products-template.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="sv">
+  <head>
+     <meta charset="utf-8">
+     <title>Produkter</title>
+		 <link rel="stylesheet" href="css/stilmall.css">
+  </head>
+  <body id="produkter">
+    <div id="wrapper">
+	
+	<?php 
+		require "masthead.php";
+		require "meny.php";
+		require "../include/connect.php";
+		
+		
+		$sql = "SELECT * FROM products";
+		
+		$res=$dbh->prepare($sql);
+		$res->execute();
+		$result=$res->get_result();
+		$dbh->close();
+	?>
+		
+		
+		<main> <!--Huvudinnehåll-->
+			<section id="content">
+				<h2>Varor</h2>
+				<table>	
+					<thead>
+						<tr>
+							<th>Namn</th>
+							<th>Beskrivning</th>
+							<th>Bild</th>
+							<th>Pris</th>
+							<th></th>
+						</tr>
+					</thead>
+					<tbody>
+					<?php
+					foreach($row as $result-> fetch_assoc()){
+						echo"<tr><td>"; 
+						echo $row['name'];
+						echo"<td><td>";
+						echo $row['description'];
+						echo"<td><td>";	
+						echo $row['picture'];
+						echo"<td><td>";
+						echo $row['price'];
+					}
+						<!--<tr>
+							<td>Äpple</td>
+							<td>Grönt surt</td>
+							<td><img src="bilder/apple.jpg" alt="Grönt surt"></td>
+							<td>50</td>
+							<td><a href="#">Köp</a></td>
+						</tr>
+						<tr>
+							<td>Apelsin</td>
+							<td>Orange söt</td>
+							<td><img src="bilder/orange.jpg" alt="Orange söt"></td>
+							<td>38</td>
+							<td><a href="#">Köp</a></td>
+						</tr>
+						<tr>
+							<td>Päron</td>
+							<td>Gult saftigt</td>
+							<td><img src="bilder/pear.jpg" alt="Gult saftigt"></td>
+							<td>100</td>
+							<td><a href="#">Köp</a></td>
+						</tr>
+						<tr>
+							<td>Banan</td>
+							<td>Gul böjd</td>
+							<td><img src="bilder/banana.jpg" alt="Gul böjd"></td>
+							<td>30</td>
+							<td><a href="#">Köp</a></td>
+						</tr>-->
+					</tbody>
+				</table>
+
+			</section>
+	
+		<?php 
+			require "footer.php";
+		?>
+	</div>
+  </body>
+</html>
