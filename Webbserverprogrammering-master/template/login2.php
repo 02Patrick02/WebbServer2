02ad9c8ef75183c2120184d1934b018d6d16f65f
02Patrick02 <54435353+02Patrick02@users.noreply.github.com>
2021-01-12 15:00:41 +0100

    Update login2.php

--- Webbserverprogrammering-master/template/login2.php
+++ Webbserverprogrammering-master/template/login2.php
@@ -1,31 +1,38 @@
 <?php
-require "../include/connect.php";
-if(empty($_POST['username'])||empty($_POST['password'])){
-	header("Location:login.php");
+
+if(empty($_POST['username'])||empty($POST['password'])){
+    header("Location:login.php");
 }
 
-$username = filter_input(INPUT_POST,'username',FILTER_SANITIZE_STRING,FILTER_FLAG_STRIP_LOW);
-$password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING_FILTER_FLAG_STRIP_LOW);
+require "../includes/connect.php";
+
+$username = filter_input(INPUT_POST,'username',FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
+$password = filter_input(INPUT_POST,'password',FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
 
 $sql="SELECT password, status FROM users WHERE username=?";
 $res=$dbh->prepare($sql);
 $res->bind_param("s", $username);
 $res->execute();
+
 $result=$res->get_result();
 $row=$result->fetch_assoc();
 
-if(!row){
-	header("Location:login.php?status=1");
+if(!$row){
+    header("Location:../html/login.php?status=1");
+}
+else{
+    if($password === $row['password']){
+        session_start();
+        $_SESSION['username']=$username;
+        $_SESSION['status']=$row['status'];
+        header("Location:../html/admin.php");
+    }
+    else{
+        header("Location:../html/login.php?status=2");
+    }
 }
 
-else{
-	if($row['password']==$password){
-		session_start();
-		$_SESSION['username']=$username;
-		header("Location:admin.php");
-	}
-	else{
-		header("Location:login.php?status=2");
-	}
-}
+echo $username;
+echo "<br>";
+echo $password;
 ?>
